8b0cc2585345e5ff07371ad685c2812a15097317
Roger Campos <andre.racampos@gmail.com>
2024-02-22 23:40:48 -0600

    Ultimo

--- js/main.js
+++ js/main.js
@@ -14,57 +14,7 @@ $(document).ready(function() {
       document.getElementById('horas').innerText = Math.floor((distance % (day)) / (hour)),
       document.getElementById('minutos').innerText = Math.floor((distance % (hour)) / (minute)),
       document.getElementById('segundos').innerText = Math.floor((distance % (minute)) / second);
-    }, second)
-
-    //Scroll parallax
-    function scrollFooter(scrollY, heightFooter)
-    {
-        if(scrollY >= heightFooter)
-        {
-            $('footer').css({
-                'bottom' : '0px'
-            });
-        }
-        else
-        {
-            $('footer').css({
-                'bottom' : '-' + heightFooter + 'px'
-            });
-        }
-    }
-
-    $(window).load(function(){
-        var windowHeight        = $(window).height(),
-            footerHeight        = $('footer').height(),
-            heightDocument      = (windowHeight) + ($('.content').height()) + ($('footer').height()) - 480;
-
-        // Definir el tamaño del elemento a animar.
-        $('#scroll-animate, #scroll-animate-main').css({
-            'height' :  heightDocument + 'px'
-        });
-
-        // Establecer el tamaño del encabezado y los elementos de contenido
-        $('.main-container').css({
-            'margin-top' : '480px'
-        });
-
-        scrollFooter(window.scrollY, footerHeight);
-
-        // Al desplazarse
-        window.onscroll = function(){
-            var scroll = window.scrollY;
-
-            $('#scroll-animate-main').css({
-                'top' : '-' + scroll + 'px'
-            });
-
-            $('header').css({
-                'background-position-y' : 50 - (scroll * 100 / heightDocument) + '%'
-            });
-
-            scrollFooter(scroll, footerHeight);
-        }
-    });
+    }, second);
 
     //reproducir audio
     var audio = document.getElementById("audio");
